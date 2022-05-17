b9ea9e45153f2bd9879086d024ef9de5ec652630
GoldenLin9 <goldenlin0909@gmail.com>
2022-05-17 02:03:57 -0400

    Added django-heroku

--- golden_games/settings.py
+++ golden_games/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+django_heroku.setting(locals())
