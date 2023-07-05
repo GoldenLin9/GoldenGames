2d3dd3ab808ea2534eaab39b6ebcbe9648e1a120
Golden Lin <67626928+GoldenLin9@users.noreply.github.com>
2023-07-04 21:36:12 -0400

    Update settings.py

--- golden_games/settings.py
+++ golden_games/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["Golden9.pythonanywhere.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
